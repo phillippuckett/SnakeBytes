0c1fe61b77d0cc0a0fd484a689519174fffab7f8
Phillip Puckett <phillippuckett88@gmail.com>
2015-12-30 11:42:42 -0700

    Beta Mode

--- grid.js
+++ grid.js
@@ -1,142 +1,114 @@
-$(document).ready(function () {
-	//Canvas stuff
-	var canvas = $("#canvas")[0];
-	var ctx = canvas.getContext("2d");
-	var w = $("#canvas").width();
-	var h = $("#canvas").height();
-	
-	//Lets save the cell width in a variable for easy control
-	var cw = 15;
-	var d;
-	var food;
-	var score;
-	
-	//Lets create the snake now
-	var snake_array; //an array of cells to make up the snake
-	
-	function init() {
-		d = "right"; //default direction
-		create_snake();
-		create_food(); //Now we can see the food particle
-		//finally lets display the score
-		score = 0;
-		
-		//Lets move the snake now using a timer which will trigger the paint function
-		//every 60ms
-		if (typeof game_loop != "undefined") clearInterval(game_loop);
-		game_loop = setInterval(paint, 60);
-	}
-	init();
+// constants //
+var COLS = 26;
+var ROWS = 26;
 
-	function create_snake() {
-		var length = 5; //Length of the snake
-		snake_array = []; //Empty array to start with
-		for (var i = length - 1; i >= 0; i--) {
-			//This will create a horizontal snake starting from the top left
-			snake_array.push({ x: i, y: 0 });
-		}
-	}
-	
-	//Lets create the food now
-	function create_food() {
-		food = {
-			x: Math.round(Math.random() * (w - cw) / cw),
-			y: Math.round(Math.random() * (h - cw) / cw),
-		};
-		//This will create a cell with x/y between 0-44
-		//Because there are 45(450/10) positions accross the rows and columns
-	}
-	
-	//Lets paint the snake now
-	function paint() {
-		//To avoid the snake trail we need to paint the BG on every frame
-		//Lets paint the canvas now
-		ctx.fillStyle = "forestgreen";
-		ctx.fillRect(0, 0, w, h);
-		ctx.strokeStyle = "brown";
-		ctx.strokeRect(0, 0, w, h);
-		
-		//The movement code for the snake to come here.
-		//The logic is simple
-		//Pop out the tail cell and place it infront of the head cell
-		var nx = snake_array[0].x;
-		var ny = snake_array[0].y;
-		//These were the position of the head cell.
-		//We will increment it to get the new head position
-		//Lets add proper direction based movement now
-		if (d == "right") nx++;
-		else if (d == "left") nx--;
-		else if (d == "up") ny--;
-		else if (d == "down") ny++;
-		
-		//Lets add the game over clauses now
-		//This will restart the game if the snake hits the wall
-		//Lets add the code for body collision
-		//Now if the head of the snake bumps into its body, the game will restart
-		if (nx == -1 || nx == w / cw || ny == -1 || ny == h / cw || check_collision(nx, ny, snake_array)) {
-			//restart game
-			init();
-			//Lets organize the code a bit now.
-			return;
-		}
-		
-		//Lets write the code to make the snake eat the food
-		//The logic is simple
-		//If the new head position matches with that of the food,
-		//Create a new head instead of moving the tail
-		if (nx == food.x && ny == food.y) {
-			var tail = { x: nx, y: ny };
-			score++;
-			//Create new food
-			create_food();
-		}
-		else {
-			var tail = snake_array.pop(); //pops out the last cell
-			tail.x = nx; tail.y = ny;
-		}
-		//The snake can now eat the food.
-		
-		snake_array.unshift(tail); //puts back the tail as the first cell
-		
-		for (var i = 0; i < snake_array.length; i++) {
-			var c = snake_array[i];
-			//Lets paint 10px wide cells
-			paint_cell(c.x, c.y);
-		}
-		
-		//Lets paint the food
-		paint_cell(food.x, food.y);
-		//Lets paint the score
-		var score_text = "Score: " + score;
-		ctx.fillText(score_text, 5, h - 5);
-	}
-	
-	//Lets first create a generic function to paint cells
-	function paint_cell(x, y) {
-		ctx.fillStyle = "darkblue";
-		ctx.fillRect(x * cw, y * cw, cw, cw);
-		ctx.strokeStyle = "forestgreen";
-		ctx.strokeRect(x * cw, y * cw, cw, cw);
-	}
+// id's //
+var EMPTY = 0;
+var SNAKE = 1;
+var FRUIT = 2;
 
-	function check_collision(x, y, array) {
-		//This function will check if the provided x/y coordinates exist
-		//in an array of cells or not
-		for (var i = 0; i < array.length; i++) {
-			if (array[i].x == x && array[i].y == y)
-				return true;
-		}
-		return false;
-	}
-	
-	//Lets add the keyboard controls now
-	$(document).keydown(function (e) {
-		var key = e.which;
-		//We will add another clause to prevent reverse gear
-		if (key == "37" && d != "right") d = "left";
-		else if (key == "38" && d != "down") d = "up";
-		else if (key == "39" && d != "left") d = "right";
-		else if (key == "40" && d != "up") d = "down";
-		//The snake is now keyboard controllable
-	})
-})
+// directions //
+var LEFT = 0;
+var UP = 1;
+var RIGHT = 2;
+var DOWN = 3;
+
+// grid dimensions //
+var grid = {
+    width: null,
+    height: null,
+    _grid: null,
+    
+    // methods //
+    init: function (d, c, r) {
+        this.width = c;
+        this.height = r;
+        this._grid = [];
+        for (var x = 0; x < c; x++) {
+            this._grid.push([]);
+            for (var y = 0; y < r; y++) {
+                this._grid.push(d);
+            }
+        }
+    },
+    set: function (val, x, y) {
+        this._grid[x][y] = val;
+    },
+    get: function (x, y) {
+        return this._grid[x][y];
+    }
+};
+var snake = {
+    direction: null,
+    last: null,
+    _queue: null,
+
+    init: function (x, y) {
+        this.direction = d;
+        this.queue = [];
+        this.insert(x, y);
+    },
+    insert: function (x, y) {
+        this._queue.unshift({ x: x, y: y });
+        this.last = this.queue[0];
+    },
+    remove: function () {
+        return this._queue[0];
+    }
+};
+
+var setFood = function () {
+    var empty = [];
+    for (var x = 0; x < grid.width; x++) {
+        for (var y = 0; y < grid.height; y++) {
+            if (grid.get(x, y) === EMPTY) {
+                empty.push({ x: x, y: y });
+            }
+        }
+    }
+    var randPos = empty[Math.floor(Math.floor.random() * empty.length)];
+    grid.set(FRUIT, randPos.x, randPos.y);
+};
+//game objects
+var canvas;
+var ctx;
+var keystate;
+var frames;
+
+function main() {
+    canvas = document.createElement("canvas");
+    canvas.width = COLS * 20;
+    canvas.height = ROWS * 20;
+    ctx = canvas.getContext("2");
+    document.body.appendChild(canvas);
+
+    frames = 0;
+    keystate = {};
+
+    init();
+    loop();
+};
+var init = function () {
+    grid.init(EMPTY, COLS, ROWS);
+
+    // stop position //
+    var sp = { x: Math.floor(COLS / 2), y: ROWS - 1 };
+    snake.init(UP, sp.x, sp.y);
+    grid.set(SNAKE, sp.x, sp.y);
+
+    setFood();
+};
+var main = function () {
+};
+var loop = function () {
+    update();
+    draw();
+    window.requestAnimationFrame(loop, canvas);
+};
+var update = function () {
+    frames++;
+};
+var draw = function () {
+    // tie width //
+    var tw =
+};
