bd43953c2c1277ce8660241227940484d69cac7e
Phillip Puckett <phillippuckett88@gmail.com>
2016-03-24 01:49:33 -0600

    lots of changes

---
+++ js/controllers.js
@@ -0,0 +1,25 @@
+angular.module('snakeBytes')
+    .controller('gameCtrl', function ($scope, $window) {
+        // $scope.gameHeader = "GAME HEADER";
+        // console.log("GAME CONTROLLER");
+    });
+
+angular.module('snakeBytes')
+    .controller('mainCtrl', function ($scope) {
+        // $scope.gameHeader = "MAIN HEADER";
+        // console.log("MAIN CONTROLLER");
+        
+        /*Toggle Option Modal--------------------*/
+        $scope.optButton = false;
+        $scope.optToggle = function () {
+            $scope.optMenu = !$scope.optMenu;
+            console.log('TOGGLE');
+        }
+    });
+
+angular.module('snakeBytes')
+    .controller('scoreCtrl', function ($scope) {
+        // $scope.gameHeader = "SCOREBOARD HEADER";
+        // console.log("SCORE CONTROLLER");
+    });
+    
---
+++ js/directives.js
@@ -0,0 +1,38 @@
+
+/** Header Directive */
+angular.module('snakeBytes')
+    .directive('hdrDir', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'html/header.html',
+            controller: function ($scope) {
+                // console.log('HEADER DIRECTIVE');
+            }
+        }
+    });
+    
+/** Footer Directive */
+angular.module('snakeBytes')
+    .directive('ftrDir', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'html/footer.html',
+            controller: function ($scope) {
+                // console.log('FOOTER DIRECTIVE');
+                var currentDate = new Date().getFullYear();
+                $scope.date = currentDate.toString();
+            }
+        }
+    });
+        
+/** Options Directive */
+// angular.module('snakeBytes')
+//     .directive('optionsDirective', function () {
+//         return {}
+//     });
+    
+/** Pause Directive */
+// angular.module('snakeBytes')
+//     .directive('pauseDirective', function () {
+//         return {}
+//     });
