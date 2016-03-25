f25a93267671fa65e8a91035e67c1d5b109a1852
Phillip Puckett <phillippuckett88@gmail.com>
2016-03-25 03:14:17 -0600

    Prepping for canvas work

--- zzz/js/directives/socialMedia/socialMediaDir.js
+++
@@ -1,7 +0,0 @@
-// angular.module('snakeBytes')
-//     .directive('socialMediaDir', function () {
-//         return {
-//             restrict: 'E',
-//             templateUrl: 'js/directives/socialMedia/socialMediaTmpl.html'
-//         }
-//     });
