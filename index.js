0bf21781c5971866045aecfbcea40ba26e4bedbf
Phillip Puckett <phillippuckett88@gmail.com>
2016-04-05 02:49:18 -0600

    Database running

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ var cors = require('cors');
 /** Express */
 var app = express();
 
+/** Connect to Front-End */
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(cors());
+
 /** Connections */
 var nodePort = 3000;
 app.listen(nodePort, function() {
