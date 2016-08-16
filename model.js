a401345e135edffad935cf159aed96b7ff7f56e5
Phillip Puckett <phillippuckett88@gmail.com>
2016-08-15 22:24:57 -0600

    mass debugging

--- model.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var scoreSchema = new Schema({
-    player: { type: String },
-    points: { type: Number }
-});
-
-module.exports = mongoose.model('scores', scoreSchema);
